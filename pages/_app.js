e5fc74487df2fe86fa70b972a3492dc979345aed
l.bruni <luigi.bruni@piksel.it>
2021-09-21 12:21:00 +0200

    init

---
+++ pages/_app.js
@@ -0,0 +1,35 @@
+import dynamic from 'next/dynamic'
+import { TinaEditProvider } from 'tinacms/dist/edit-state'
+const TinaCMS = dynamic(() => import('tinacms'), { ssr: false })
+import '../styles/index.css'
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <TinaEditProvider
+        editMode={
+          <TinaCMS
+            clientId={process.env.NEXT_PUBLIC_TINA_CLIENT_ID}
+            branch={process.env.NEXT_PUBLIC_EDIT_BRANCH}
+            organization={process.env.NEXT_PUBLIC_ORGANIZATION_NAME}
+            isLocalClient={Boolean(
+              Number(process.env.NEXT_PUBLIC_USE_LOCAL_CLIENT ?? true)
+            )}
+            cmsCallback={cms => {
+              import('react-tinacms-editor').then((field) => {
+                cms.plugins.add(field.MarkdownFieldPlugin)
+              })
+            }}
+            {...pageProps}
+          >
+            {(livePageProps) => <Component {...livePageProps} />}
+          </TinaCMS>
+        }
+      >
+        <Component {...pageProps} />
+      </TinaEditProvider>
+    </>
+  )
+}
+
+export default App
---
+++ pages/posts/[slug].js
@@ -0,0 +1,127 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import { useEffect, useState } from 'react'
+import Container from '../../components/container'
+import PostBody from '../../components/post-body'
+import Header from '../../components/header'
+import PostHeader from '../../components/post-header'
+import Layout from '../../components/layout'
+import PostTitle from '../../components/post-title'
+import Head from 'next/head'
+import { staticRequest, getStaticPropsForTina } from 'tinacms'
+import { CMS_NAME } from '../../lib/constants'
+import markdownToHtml from '../../lib/markdownToHtml'
+
+export default function Post({ data, slug }) {
+  const {
+    title,
+    coverImage,
+    date,
+    author,
+    body,
+    ogImage
+  } = data.getPostDocument.data
+
+  const router = useRouter()
+  const [content, setContent] = useState('')
+  useEffect(() => {
+    const parseMarkdown = async () => {
+      setContent(await markdownToHtml(body))  
+    }
+
+    parseMarkdown()
+  }, [body]);
+
+  if (!router.isFallback && !slug) {
+    return <ErrorPage statusCode={404} />
+  }
+  return (
+    <Layout preview={false}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article className="mb-32">
+              <Head>
+                <title>
+                  {title} | Next.js Blog Example with {CMS_NAME}
+                </title>
+                <meta property="og:image" content={ogImage.url} />
+              </Head>
+              <PostHeader
+                title={title}
+                coverImage={coverImage}
+                date={date}
+                author={author}
+              />
+              <PostBody content={content} />
+            </article>
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const variables = { relarivePath: `${slug}.md` };
+  const tinaProps = await getStaticPropsForTina({
+    query: `
+      query BlogPostQuery($relativePath: String!) {
+        getPostsDocument(relativePath: $relativePath) {
+          data {
+            title
+            excerpt
+            date
+            coverImage
+            author {
+              name
+              picture
+            }
+            ogImage {
+              url
+            }
+            body
+          }
+        }
+      }
+    `,
+    variables
+  })
+
+  return {
+    props: {
+      ...tinaProps,
+      slug,
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const postListData = staticRequest({
+    query: `
+      query {
+        getPostsList {
+          edges {
+            node {
+              sys {
+                filename
+              }
+            }
+          }
+        }
+      }
+    `,
+    variables: {},
+  })
+
+  return {
+    paths: postListData.getPostsList.edges.map(edge => ({
+      params: { slug: edge.node.sys.filename },
+    })),
+    fallback: false
+  };
+}
